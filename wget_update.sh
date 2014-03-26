5ba6afac076b90bdd76b4293d68da37fb20308b5
Andrew Roslik <andkirby@gmail.com>
2014-03-26 16:36:26 +0200

    - Added file for the getting last version of files files by wget.

---
+++ wget_update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+wget --no-check-certificate https://github.com/andkirby/mageinstall/archive/master.tar.gz -O master.tar.gz
+tar -xvzf $SCRIPT_DIR"/master.tar.gz" mageinstall-master
+bash -c "cp -R "$SCRIPT_DIR"/mageinstall-master/* "$SCRIPT_DIR
+bash -c "rm -rf "$SCRIPT_DIR"/mageinstall-master"
+bash -c "rm -rf "$SCRIPT_DIR"/master.tar.gz"
