ed4753278ce9796bcb146c8c677ae42893a6225c
Andrew Roslik <andkirby@gmail.com>
2014-04-03 23:10:38 +0300

    - Fixed update files via wget from another directory.

--- wget_update.sh
+++ wget_update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
-wget --no-check-certificate https://github.com/andkirby/mageinstall/archive/master.tar.gz -O master.tar.gz
+wget --no-check-certificate https://github.com/andkirby/mageinstall/archive/master.tar.gz -O $SCRIPT_DIR"/master.tar.gz"
 tar -xvzf $SCRIPT_DIR"/master.tar.gz" mageinstall-master
 bash -c "cp -R "$SCRIPT_DIR"/mageinstall-master/* "$SCRIPT_DIR
 bash -c "rm -rf "$SCRIPT_DIR"/mageinstall-master"
