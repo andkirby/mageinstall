2f5547d656bc8399758681dca99f431eb1853f46
Andrew Roslik <andkirby@gmail.com>
2014-04-08 03:54:04 +0300

    - Added checking wget.

--- wget_update.sh
+++ wget_update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+command -v wget >/dev/null 2>&1 || { echo >&2 "Script require \"wget\" but it's not installed. Aborting."; exit 1; }
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 wget --no-check-certificate https://github.com/andkirby/mageinstall/archive/master.tar.gz -O $SCRIPT_DIR"/master.tar.gz"
 tar -xvzf $SCRIPT_DIR"/master.tar.gz" mageinstall-master
