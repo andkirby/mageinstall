78b4e46f636a5b2cec99ba669e9771dbc7ace24b
Andrew Roslik <andkirby@gmail.com>
2014-06-06 06:41:15 +0300

    - Added ability to run extra scripts from sample data directory.

--- tools/import.sh
+++ tools/import.sh
@@ -10,13 +10,33 @@ then
 #        cp "$IMPORT_DIR/media/*" "$PROJECT_DIR/media/import"
 #    fi
 
+    RESULT=""
+    if [ -d "$SAMPLE_DATA_DIR/scripts" ] && [ -f "$SAMPLE_DATA_DIR/scripts/import-categories.sh" ]
+    then
+        . "$SAMPLE_DATA_DIR/scripts/import-categories.sh"
+        REINDEX_ALL_RUN="1"
+    fi
+
+    if [ -d "$SAMPLE_DATA_DIR/scripts" ] && [ -f "$SAMPLE_DATA_DIR/scripts/import-products-before.sh" ]
+    then
+        . "$SAMPLE_DATA_DIR/scripts/import-products-before.sh"
+        REINDEX_ALL_RUN="1"
+    fi
+
     FILE_CSV=""
     for FILE_CSV in $IMPORT_DIR/*.csv; do
         echo "Importing products file $FILE_CSV..."
         $PHP_BIN -f "$SCRIPT_DIR/tools/import.php" "\"$PROJECT_DIR\"" "\"$FILE_CSV\""
+        REINDEX_ALL_RUN="1"
     done
 
-    if [ "$FILE_CSV" ] # check if any file participated in the importing
+    if [ -d "$SAMPLE_DATA_DIR/scripts" ] && [ -f "$SAMPLE_DATA_DIR/scripts/import-products-after.sh" ]
+    then
+        . "$SAMPLE_DATA_DIR/scripts/import-products-after.sh"
+        REINDEX_ALL_RUN="1"
+    fi
+
+    if [ "$FILE_CSV" ] || [ "$REINDEX_ALL_RUN" ] # check if any file participated in the importing
     then
         echo "Reindexing..."
         $PHP_BIN -f $PROJECT_DIR/shell/indexer.php reindexall
