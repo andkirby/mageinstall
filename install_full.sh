280c6067d5b310414cb38d8c57a4345679e8e881
Andrew Roslik <andkirby@gmail.com>
2015-04-15 17:09:57 +0300

    - Fixed SQL to remove all tables.

--- install_full.sh
+++ install_full.sh
@@ -80,17 +80,18 @@ then
     echo "Refresh database '$DB_NAME'..."
     $DB_CONNECT_COMMAND -h$DB_HOST -e "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;"
     $DB_CONNECT_COMMAND -h$DB_HOST -e "
-SET FOREIGN_KEY_CHECKS = 0;
-SET @tables = NULL;
-SELECT GROUP_CONCAT(table_schema, '.', table_name) INTO @tables
-  FROM information_schema.tables
-  WHERE table_schema = 'database_name'; -- specify DB name here.
-
-SET @tables = CONCAT('DROP TABLE ', @tables);
-PREPARE stmt FROM @tables;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
-SET FOREIGN_KEY_CHECKS = 1;"
+        SET FOREIGN_KEY_CHECKS = 0;
+        SELECT DATABASE() FROM DUAL INTO @current_dbname;
+        SET @tables = NULL;
+        SET SESSION group_concat_max_len = 1000000;
+        SELECT GROUP_CONCAT(table_schema, '.', table_name) INTO @tables
+          FROM information_schema.tables
+          WHERE table_schema = @current_dbname; -- specify DB name here.
+        SET @tables = CONCAT('DROP TABLE ', @tables);
+        PREPARE stmt FROM @tables;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+        SET FOREIGN_KEY_CHECKS = 1;"
     echo "Database refreshed."
 fi
 
