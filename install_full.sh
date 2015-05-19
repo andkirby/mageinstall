a52df20efd302b501198eaa418dd1a482bb5c1ad
Andrew Roslik <andkirby@gmail.com>
2015-05-19 18:49:19 +0300

    - Added supporting missed $SAMPLE_DATA_MEDIA_RUN parameter.

--- install_full.sh
+++ install_full.sh
@@ -98,7 +98,7 @@ then
     echo "Database refreshed."
 fi
 
-# ======= Install Sample Data =======
+# ======= Install Sample SQL Data =======
 if [ true = "$SAMPLE_DATA_SQL_RUN" ] && [ -d "$SAMPLE_DATA_DIR/sample" ]
 then
     echo "Installing sample data SQL files..."
@@ -106,8 +106,14 @@ then
         $DB_CONNECT_COMMAND -h$DB_HOST $DB_NAME < $SQL_FILE
         echo "Added to DB SQL file: $SQL_FILE..."
     done
+else
+    echo "Skipped adding sample SQL data."
+fi
 
-    echo "Installing sample data files..."
+# ======= Install Sample Data files =======
+if [ true = "$SAMPLE_DATA_MEDIA_RUN" ] && [ -d "$SAMPLE_DATA_DIR/sample" ]
+then
+    echo "Copying sample data files..."
     for i in "$SAMPLE_DATA_DIR/sample"/* ; do
         if [ -d "$i" ]; then
             DIR=$(basename "$i")
@@ -120,7 +126,7 @@ then
         fi
     done
 else
-    echo "Skipped adding sample data."
+    echo "Skipped copying sample data files."
 fi
 
 # ======== Install Magento ========
