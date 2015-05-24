81cce9a3f36c8eafd5508d069acbe3f5155581c8
Andrew Roslik <andkirby@gmail.com>
2015-05-24 22:09:00 +0300

    - Added build script.

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
-SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
-cd "$SCRIPT_DIR"
+SRC_DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && cd .. && pwd)
+cd "$SRC_DIR"
 
-echo "Error: Building not implemented yet."
+if [ ! -f ~/.mageinstall/params.sh ] ; then
+    echo "Error: You have to initialize install params. Try 'mageshell install init'."
+    exit 1
+fi
+
+# load functions
+. "$SRC_DIR/lib/function.sh"
+
+# include default params
+. "$SRC_DIR/install/params.sh.dist"
+
+# get CLI options
+. "$SRC_DIR/build/lib/getopt.sh"
+
+# include custom params
+. "$SRC_DIR/install/load-params.sh"
+
+if [ -f ~/.mageinstall/build/params.sh ] ; then
+    . ~/.mageinstall/build/params.sh
+fi
+
+if [ ! -f ~/.mageinstall/build/composer.json ] ; then
+    . "$SRC_DIR/build/lib/make-composer-json.sh"
+fi
---
+++ src/build/lib/getopt.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# (POSIX shell syntax)
+
+while :
+do
+    case $1 in
+        -h | --help | -\?)
+            # Show some help
+            out=$(cat doc/shell/readme)
+            echo -e "$out"
+            exit 0 # This is not an error, User asked help. Don't do "exit 1"
+            ;;
+        -p | --project)
+            PROJECT="$2"
+            shift 2
+            ;;
+        -P | --project-http-dir)
+            PROJECT_DIR="$2"
+            shift 2
+            ;;
+        -m | --magento-source-dir)
+            MAGENTO_DIR="$2"
+            shift 2
+            ;;
+        -R | --refresh-magento-files)
+            setBoolean MAGENTO_REFRESH "$2"
+            shift 2
+            ;;
+        -k | --package-dir)
+            PACKAGE_DIR="$2"
+            shift 2
+            ;;
+        -g | --package)
+            PACKAGE="$2"
+            shift 2
+            ;;
+        -i | --install-run)
+            setBoolean INSTALL_RUN "$2"
+            shift 2
+            ;;
+        -*)
+            printf >&2 'WARN: Unknown option (ignored): %s\n' "$1"
+            shift
+            ;;
+        *)  # no more options. Stop while loop
+            break
+            ;;
+        --) # End of all options
+        echo "End of all options"
+            shift
+            break
+            ;;
+    esac
+done
