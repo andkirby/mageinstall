d7636a9407fb5d5ec5d4a3005274d3d9871e9be9
Andrew Roslik <andkirby@gmail.com>
2015-05-25 02:49:48 +0300

    - Fixed catching exceptions.

--- src/build/lib/package-install.sh
+++ src/build/lib/package-install.sh
@@ -5,7 +5,7 @@ RESULT=$(cd "$PACKAGE_DIR" && composer require "$PACKAGE" 2>&1)
 # show result
 echo "$RESULT"
 
-hasError=$(echo "$RESULT" | grep -E "(failed|fatal|Error)" 2>&1)
+hasError=$(echo "$RESULT" | grep -E "(failed|fatal|Error|Exception)" 2>&1)
 if [ "$hasError" ] ; then
     # stop if error
     exit 1
