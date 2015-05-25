d7caa0f9bc96d7019e835a35c3ea3195ccaeb34b
Andrew Roslik <andkirby@gmail.com>
2015-05-25 17:51:47 +0300

    - Fixed help for install. - Added logo for build.

--- src/install/lib/getopt.sh
+++ src/install/lib/getopt.sh
@@ -6,7 +6,7 @@ do
     case $1 in
         -h | --help)
             # Show some help
-            out=$(cat ../../doc/shell/logo)"\n"$(cat doc/shell/help)"\n"
+            out=$(cat "$SRC_DIR"/doc/shell/logo)"\n"$(cat "$SRC_DIR"/install/doc/shell/help)"\n"
             echo -e "$out"
             exit 0 # This is not an error, User asked help. Don't do "exit 1"
             ;;
