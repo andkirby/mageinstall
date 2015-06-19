6a7d5f8d81964519242b43d486ff0cfbadcd0b83
Andrew Roslik <andkirby@gmail.com>
2015-06-19 13:48:55 +0300

    - Added params to add project config files.

--- src/install/magento-config.sh
+++ src/install/magento-config.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-# Add configuration into Magento instance
+# Add sample data configuration into Magento instance
 if [ "$SAMPLE_DATA_CONFIG_RUN" = true ] && [ -d "$SAMPLE_DATA_DIR" ]
 then
     for FILE_INI in "$SAMPLE_DATA_DIR"/*.csv; do
+        echo "Applying configuration from file $FILE_INI..."
+        $PHP_BIN -f "$SRC_DIR/install/lib/config.php" "$FILE_INI"
+    done
+fi
+
+# Add project configuration into Magento instance
+if [ "$PROJECT_CONFIG_RUN" = true ] && [ -d "$PROJECT_CONFIG_DIR" ]
+then
+    for FILE_INI in $(find $PROJECT_CONFIG_DIR -name "*.csv" | sort); do
         echo "Applying configuration from file $FILE_INI..."
         $PHP_BIN -f "$SRC_DIR/install/lib/config.php" "$FILE_INI"
     done
