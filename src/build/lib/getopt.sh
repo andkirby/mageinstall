40b191c8405504f16acc03b41611f73ec956c60a
Andrew Roslik <andkirby@gmail.com>
2015-06-19 15:24:40 +0300

    - Added verbosity params.

--- src/build/lib/getopt.sh
+++ src/build/lib/getopt.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # (POSIX shell syntax)
 PACKAGE_COMPOSER_URL=()
+VERBOSITY=false
+VERBOSITY_VERY=false
+VERBOSITY_VERY_VERY=false
+
 while :
 do
     case $1 in
@@ -50,15 +54,30 @@ do
             setBoolean INSTALL_PARAMS "$2"
             shift 2
             ;;
+        -v)
+            VERBOSITY=true
+            shift 1
+            ;;
+        -vv)
+            VERBOSITY=true
+            VERBOSITY_VERY=true
+            shift 1
+            ;;
+        -A | -vvv)
+            VERBOSITY=true
+            VERBOSITY_VERY=true
+            VERBOSITY_VERY_VERY=true
+            shift 1
+            ;;
         -*)
-            printf >&2 'WARN: Unknown option (ignored): %s\n' "$1"
+            printf >&2 'WARN: Unknown build option (ignored): %s\n' "$1"
             shift
             ;;
         *)  # no more options. Stop while loop
             break
             ;;
         --) # End of all options
-        echo "End of all options"
+        echo "End of all build options"
             shift
             break
             ;;
