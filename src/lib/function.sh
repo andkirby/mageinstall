75a6b2b408d9dda653fdd563f841b3e3b043d653
Andrew Roslik <andkirby@gmail.com>
2015-06-19 15:21:47 +0300

    - Added die function.

--- src/lib/function.sh
+++ src/lib/function.sh
@@ -3,14 +3,27 @@
 function setBoolean {
     local v
     if (( $# != 2 )); then
-     echo "Error: Improper setBoolean() usage" 1>&2; exit 1 ;
+        die "Improper setBoolean() usage" 1>&2;
     fi
 
     case "$2" in
         TRUE | true | yes | YES | 1) v=true ;;
         FALSE | false | no | NO | 0 | "") v=false ;;
-        *) echo "Error: Unknown boolean value \"$2\". Please use 'true|false' or 'yes|no' or '1|0' or '' for 'false'." 1>&2; exit 1 ;;
+        *) die "Unknown boolean value \"$2\". Please use 'true|false' or 'yes|no' or '1|0' or '' for 'false'." 1>&2;
+            ;;
     esac
 
     eval $1=$v
+}
+
+function die {
+    echo "Error: $*"
+    echo "$VERBOSITY_VERY_VERY"
+    if [ "$VERBOSITY_VERY_VERY" = "true" ] ; then
+        local frame=0
+        while caller "$frame"; do
+            ((frame++));
+        done
+    fi
+    exit 1
 }
