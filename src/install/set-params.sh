96e71b2aa27ac92685b66c3307412f34fa8ddf6c
Andrew Roslik <andkirby@gmail.com>
2015-06-24 13:21:31 +0300

    - Refactored.  - Removed redundant question.  - Added extra params to dialog.

--- src/install/set-params.sh
+++ src/install/set-params.sh
@@ -10,7 +10,13 @@ fi
 . "$SRC_DIR/lib/function.sh"
 
 function getUserParam() {
-    local PROJECT DB_HOST DB_USER DB_PASSWORD DB_NAME PROJECT_DOMAIN_MASK PROTOCOL_SECURED USE_REWRITES ADMIN_USERNAME ADMIN_PASSWORD ADMIN_EMAIL INSTALL_RUN IMPORT_RUN SAMPLE_DATA_SQL_RUN SAMPLE_DATA_MEDIA_RUN SAMPLE_DATA_CONFIG_RUN ROOT SAMPLE_DATA_DIR IMPORT_DIR PROJECT_DIR MYSQL_BIN PHP_BIN
+    local PROJECT DB_HOST DB_USER DB_PASSWORD DB_NAME PROJECT_DOMAIN_MASK \
+        PROTOCOL_SECURED USE_REWRITES ADMIN_USERNAME ADMIN_PASSWORD ADMIN_EMAIL \
+        INSTALL_RUN IMPORT_RUN SAMPLE_DATA_SQL_RUN SAMPLE_DATA_MEDIA_RUN \
+        SAMPLE_DATA_CONFIG_RUN ROOT SAMPLE_DATA_DIR IMPORT_DIR PROJECT_DIR \
+        MYSQL_BIN PHP_BIN MEDIA_DIR_PERMISSIONS MEDIA_DIR_USER PROJECT_CONFIG_DIR \
+        PROJECT_CONFIG_RUN
+
     if [ -f $(cd ~; pwd)"/.mageinstall/params.sh" ] ; then
         . ~/.mageinstall/params.sh
     fi
@@ -25,9 +31,10 @@ echo "__SAMPLE_DIR__ - Sample data directory."
 echo ""
 
 params=(\
-"DB_HOST:DB hostname" \
-"DB_USER:DB username" \
-"DB_PASSWORD:Database password" \
+"DB_HOST:Database server hostname" \
+"DB_NAME:Database name" \
+"DB_USER:Database server username" \
+"DB_PASSWORD:Database server password" \
 "PROJECT_DOMAIN_MASK:Domain pattern. Use __PROJECT__ to put project name automatically" \
 "PROTOCOL_SECURED:Secured protocol. Use 'http' if you do not have HTTPS:http|https" \
 "USE_REWRITES:Use Apache URL rewrites:yes|no" \
@@ -40,6 +47,8 @@ params=(\
 "PROJECT_CONFIG_RUN:Run importing project configuration files:boolean" \
 "SAMPLE_DATA_SQL_RUN:Run importing sample data SQL files:boolean" \
 "SAMPLE_DATA_MEDIA_RUN:Run importing sample data media files:boolean" \
+"MEDIA_DIR_PERMISSIONS:Permissions for 'var', 'media', and 'etc' directories:0777" \
+"MEDIA_DIR_USER:Permissions user for 'var', 'media', and 'etc' directories" \
 "ROOT:Document Root" \
 "SAMPLE_DATA_DIR:Sample data directory" \
 "IMPORT_DIR:Product import files directory" \
@@ -47,26 +56,7 @@ params=(\
 "MYSQL_BIN:Path to MySQL client binary file" \
 "PHP_BIN:Path to PHP binary file" \
 )
-
 nonDefault=()
-while true :
-do
-    printf "Do you have several Magento projects? (yes|no) [yes] : "
-    read answer
-    case "$answer" in
-        no)
-            while [[ -z "$DB_NAME" ]] ; do
-                userParam=$(getUserParam $DB_NAME)
-                printf "Database name [$DB_NAME] : "
-                read DB_NAME
-            done
-            nonDefault+=("DB_NAME=\"$DB_NAME\"")
-            PROJECT_DOMAIN_MASK="project-__PROJECT__.localhost"
-            break
-            ;;
-        yes | "") break ;;
-    esac
-done
 
 for item in "${params[@]}"; do
 
