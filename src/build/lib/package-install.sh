5a5dc4ca9ce42e3c1690a5f10547396f867b1557
Andrew Roslik <andkirby@gmail.com>
2015-07-15 14:31:42 +0300

    - Removed skipping installing installer because new composer.json.

--- src/build/lib/package-install.sh
+++ src/build/lib/package-install.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
 #install required installer version
-FOUND=$(cd "$PACKAGE_DIR" && composer show -i | grep magento-hackathon/magento-composer-installer | grep " 3\." 2>&1)
-if [ -z "$FOUND" ] ; then
-    echo "Installing the package 'magento-hackathon/magento-composer-installer:~3.0@stable'..."
-    RESULT=$(cd "$PACKAGE_DIR" && composer require magento-hackathon/magento-composer-installer:~3.0@stable 2>&1)
-    # show result
-    echo "$RESULT"
+echo "Installing the package 'magento-hackathon/magento-composer-installer:~3.0@stable'..."
+RESULT=$(cd "$PACKAGE_DIR" && composer require magento-hackathon/magento-composer-installer:~3.0@stable 2>&1)
+# show result
+echo "$RESULT"
 
-    hasError=$(echo "$RESULT" | grep -E "(failed|fatal|Error|Exception|Problem)" 2>&1)
-    if [ "$hasError" ] ; then
-        # stop if error
-        die "Package requiring failed."
-    fi
+hasError=$(echo "$RESULT" | grep -E "(failed|fatal|Error|Exception|Problem)" 2>&1)
+if [ "$hasError" ] ; then
+    # stop if error
+    die "Package requiring failed."
 fi
 
 # Install target package
