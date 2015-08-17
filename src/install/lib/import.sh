c28b9d9af21949cbcd74b4e95cc28b393a5d3900
Andrew Roslik <andkirby@gmail.com>
2015-08-17 19:32:34 +0300

    - Updated variable boolean value.

--- src/install/lib/import.sh
+++ src/install/lib/import.sh
@@ -3,40 +3,32 @@
 # Import products
 if [ "$IMPORT_RUN" = true ] && [ -d "$IMPORT_DIR" ]
 then
-#    if [ -d "$IMPORT_DIR/media" ] ; then
-#        if [ -d "$PROJECT_DIR/media/import" ] ; then
-#            mkdir "$PROJECT_DIR/media/import"
-#        fi
-#        cp "$IMPORT_DIR/media/*" "$PROJECT_DIR/media/import"
-#    fi
-
-    RESULT=""
     user_message "$SAMPLE_DATA_DIR/scripts/import-categories.sh" 2
     if [ -d "$SAMPLE_DATA_DIR/scripts" ] && [ -f "$SAMPLE_DATA_DIR/scripts/import-categories.sh" ]
     then
         . "$SAMPLE_DATA_DIR/scripts/import-categories.sh"
-        REINDEX_ALL_RUN="1"
+        REINDEX_ALL_RUN=true
     fi
 
     user_message "$SAMPLE_DATA_DIR/scripts/import-products-before.sh" 2
     if [ -d "$SAMPLE_DATA_DIR/scripts" ] && [ -f "$SAMPLE_DATA_DIR/scripts/import-products-before.sh" ]
     then
         . "$SAMPLE_DATA_DIR/scripts/import-products-before.sh"
-        REINDEX_ALL_RUN="1"
+        REINDEX_ALL_RUN=true
     fi
 
     FILE_CSV=""
     for FILE_CSV in $IMPORT_DIR/*.csv; do
         user_message "Importing products file $FILE_CSV..." 2
         $PHP_BIN -f "$SCRIPT_DIR/lib/import.php" "\"$PROJECT_DIR\"" "\"$FILE_CSV\""
-        REINDEX_ALL_RUN="1"
+        REINDEX_ALL_RUN=true
     done
 
     user_message "$SAMPLE_DATA_DIR/scripts/import-products-after.sh" 2
     if [ -d "$SAMPLE_DATA_DIR/scripts" ] && [ -f "$SAMPLE_DATA_DIR/scripts/import-products-after.sh" ]
     then
         . "$SAMPLE_DATA_DIR/scripts/import-products-after.sh"
-        REINDEX_ALL_RUN="1"
+        REINDEX_ALL_RUN=true
     fi
 
     if [ "$REINDEX_ALL_RUN" ] # check if any file participated in the importing
