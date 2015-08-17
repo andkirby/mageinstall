7bbecb383b2d251bdf54c773750fe8946390792c
Andrew Roslik <andkirby@gmail.com>
2015-08-17 15:36:49 +0300

    - Added passing verbosity level to installing.

--- src/build/build.sh
+++ src/build/build.sh
@@ -17,10 +17,10 @@ REBUILD_START=$(date +%s)
 binDir=$(cd $SRC_DIR/../bin/; pwd)
 if [ "$PACKAGE_INSTALL_RUN" = "true" ] ; then
     echo "Installing..."
-    bash "$binDir/"mageshell install -p "$PROJECT"
+    bash "$binDir/"mageshell install -p "$PROJECT" ${VERBOSITY_PARAM}
 else
     echo "Installation will be ignored. Clear cache only."
-    bash "$binDir/"mageshell install -p "$PROJECT" -S -a 1
+    bash "$binDir/"mageshell install -p "$PROJECT" -S -a 1 ${VERBOSITY_PARAM}
 fi
 
 END=$(date +%s)
