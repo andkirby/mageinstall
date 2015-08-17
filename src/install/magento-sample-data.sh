0f4026cb4105d900167ffdf8c595c33aefd18dbf
Andrew Roslik <andkirby@gmail.com>
2015-08-17 16:13:25 +0300

    - Updated code to use user_message.

--- src/install/magento-sample-data.sh
+++ src/install/magento-sample-data.sh
@@ -2,10 +2,10 @@
 # ======= Install Sample SQL Data =======
 if [ true = "$SAMPLE_DATA_SQL_RUN" ] && [ -d "$SAMPLE_DATA_DIR/sample" ]
 then
-    echo "Installing sample data SQL files..."
+    user_message "Installing sample data SQL files..." 1
     for SQL_FILE in $SAMPLE_DATA_DIR/sample/*.sql; do
         $DB_CONNECT_COMMAND -h$DB_HOST $DB_NAME < $SQL_FILE
-        echo "Added to DB SQL file: $SQL_FILE..."
+        user_message "Added to DB SQL file: $SQL_FILE..." 1
     done
 else
     echo "Skipped adding sample SQL data."
@@ -14,16 +14,16 @@ fi
 # ======= Install Sample Data files =======
 if [ true = "$SAMPLE_DATA_MEDIA_RUN" ] && [ -d "$SAMPLE_DATA_DIR/sample" ]
 then
-    echo "Copying sample data files..."
+    user_message "Copying sample data files..." 1
     if [ "$INSTALL_RUN" = "true" ] && [ -f "$SAMPLE_DATA_DIR/sample"/app/etc/local.xml ] ; then
-        echo "Notice: You going to copy 'local.xml' file. Installation will be ignored."
+        user_message "Notice: You going to copy 'local.xml' file. Installation will be ignored." 2
         INSTALL_RUN=false
     fi
 
     for i in "$SAMPLE_DATA_DIR/sample"/* ; do
         if [ -d "$i" ]; then
             DIR=$(basename "$i")
-            echo "Copying directory sample/$DIR..."
+            user_message "Copying directory sample/$DIR..." 2
             if [ -d "$PROJECT_DIR/$DIR/" ]; then
                 cp -Rf $i/* $PROJECT_DIR/$DIR/
             else
@@ -32,5 +32,5 @@ then
         fi
     done
 else
-    echo "Skipped copying sample data files."
+    user_message "Skipped copying sample data files." 1
 fi
