efc0a4074836f104390e94ac1ea48eb18e2f892d
Andrew Roslik <andkirby@gmail.com>
2015-08-17 17:00:05 +0300

    - Updated verbosity level.

--- src/install/magento-sample-data.sh
+++ src/install/magento-sample-data.sh
@@ -2,21 +2,21 @@
 # ======= Install Sample SQL Data =======
 if [ true = "$SAMPLE_DATA_SQL_RUN" ] && [ -d "$SAMPLE_DATA_DIR/sample" ]
 then
-    user_message "Installing sample data SQL files..." 1
+    user_message "Installing sample data SQL files..." 0
     for SQL_FILE in $SAMPLE_DATA_DIR/sample/*.sql; do
         $DB_CONNECT_COMMAND -h$DB_HOST $DB_NAME < $SQL_FILE
         user_message "Added to DB SQL file: $SQL_FILE..." 2
     done
 else
-    echo "Skipped adding sample SQL data."
+    user_message "Skipped adding sample SQL data." 1
 fi
 
 # ======= Install Sample Data files =======
 if [ true = "$SAMPLE_DATA_MEDIA_RUN" ] && [ -d "$SAMPLE_DATA_DIR/sample" ]
 then
-    user_message "Copying sample data files..." 1
+    user_message "Copying sample data files..." 0
     if [ "$INSTALL_RUN" = "true" ] && [ -f "$SAMPLE_DATA_DIR/sample"/app/etc/local.xml ] ; then
-        user_message "Notice: You going to copy 'local.xml' file. Installation will be ignored." 2
+        user_message "Notice: You going to copy 'local.xml' file. Installation will be ignored." 1
         INSTALL_RUN=false
     fi
 
