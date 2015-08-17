48e771732c0a3edd9cbec20ade7e6b309055ce37
Andrew Roslik <andkirby@gmail.com>
2015-08-17 16:01:16 +0300

    - Updated code to use user_message.

--- src/build/lib/project-clean-up.sh
+++ src/build/lib/project-clean-up.sh
@@ -4,27 +4,27 @@ if [ -f ~/.mageinstall/build/before-clean.sh ] ; then
     . ~/.mageinstall/build/before-clean.sh
 fi
 if [ "$REFRESH_ALL" = "true" ] || [ ! -f "$PROJECT_DIR/app/Mage.php" ] ; then
-    echo "Cleaning up..."
-    echo "Cleaning up $PACKAGE_DIR..."
+    user_message "Cleaning up..." 1
+    user_message "Cleaning up $PACKAGE_DIR..." 2
     rm -rf "$PACKAGE_DIR"/*
-    echo "Cleaning up $PROJECT_DIR..."
+    user_message "Cleaning up $PROJECT_DIR..." 2
     rm -rf "$PROJECT_DIR"/*
 
-    echo "Copying Magento source files..."
+    user_message "Copying Magento source files..." 1
     if [ ${COPY_METHOD} = "symlink" ] ; then
-        echo "Notice: copying will use symlink."
+        user_message "Notice: copying will use symlink." 2
         cp -rl "$MAGENTO_DIR"/* "$PROJECT_DIR"/
     else
         cp -r "$MAGENTO_DIR"/* "$PROJECT_DIR"/
     fi
-    echo 'Cleaning up completed.'
+    user_message 'Cleaning up completed.' 1
 elif [ "$PACKAGE_INSTALL_RUN" = "true" ] ; then
-    echo "Clean up project directory..."
+    user_message "Clean up project directory..." 1
     filesToRemove=$(cd "$PROJECT_DIR" && find . -type f -print0 |
         grep -Fxvz -f <(cd "$MAGENTO_DIR" && find . -type f) |
         xargs -0 rm 2>&1)
     cd "$PROJECT_DIR" && find . -empty -type d -delete
-    echo 'Cleaning up completed.'
+    user_message 'Cleaning up completed.' 1
 fi
 
 if [ -f ~/.mageinstall/build/after-clean.sh ] ; then
