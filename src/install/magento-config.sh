0dd92b260a1a3704ecfece6db1bc6b8823c8fe16
Andrew Roslik <andkirby@gmail.com>
2015-08-17 15:05:40 +0300

    - Added preventing redundant errors.

--- src/install/magento-config.sh
+++ src/install/magento-config.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
 # Add sample data configuration into Magento instance
 if [ "$SAMPLE_DATA_CONFIG_RUN" = true ] && [ -d "$SAMPLE_DATA_DIR" ]
 then
     for FILE_INI in "$SAMPLE_DATA_DIR"/*.csv; do
+        if [ ${FILE_INI} = "$SAMPLE_DATA_DIR"/*.csv ] ; then
+            break
+        fi
         echo "Applying configuration from file $FILE_INI..."
         $PHP_BIN -f "$SRC_DIR/install/lib/config.php" "$FILE_INI"
     done
@@ -12,6 +16,9 @@ fi
 if [ "$PROJECT_CONFIG_RUN" = true ] && [ -d "$PROJECT_CONFIG_DIR" ]
 then
     for FILE_INI in $(find $PROJECT_CONFIG_DIR -name "*.csv" | sort); do
+        if [ ${FILE_INI} = "*.csv" ] ; then
+            break
+        fi
         echo "Applying configuration from file $FILE_INI..."
         $PHP_BIN -f "$SRC_DIR/install/lib/config.php" "$FILE_INI"
     done
