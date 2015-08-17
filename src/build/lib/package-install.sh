af8625a985c813421c5b66d0c68c84ddc77a7a63
Andrew Roslik <andkirby@gmail.com>
2015-08-17 19:26:13 +0300

    - Updated catching errors via getting status of last command.

--- src/build/lib/package-install.sh
+++ src/build/lib/package-install.sh
@@ -7,17 +7,17 @@ fi
 
 # require installer
 user_message "Adding the installer package 'magento-hackathon/magento-composer-installer:~3.0@stable'..." 2
-RESULT=$(cd "$PACKAGE_DIR" && composer require \
+result=$(cd "$PACKAGE_DIR" && composer require \
     magento-hackathon/magento-composer-installer:~3.0@stable \
     --no-update \
     $COMPOSER_VERBOSITY_PARAM \
     $INTERACTION_PARAM \
     2>&1)
+status=$?
 # show result
-user_message "$RESULT" 0
+user_message "$result" 0
 
-hasError=$(echo "$RESULT" | grep -E "(failed|fatal|Error|Exception|Problem)" 2>&1)
-if [ "$hasError" ] ; then
+if [ ${status} != 0 ] ; then
     # stop if error
     die "Package requiring failed."
 fi
@@ -25,37 +25,37 @@ fi
 user_message "Adding the package '$PACKAGE'..." 1
 
 # Require target package
-RESULT=$(cd "$PACKAGE_DIR" && composer require "$PACKAGE" \
+result=$(cd "$PACKAGE_DIR" && composer require "$PACKAGE" \
     --no-update \
     $COMPOSER_VERBOSITY_PARAM \
     $INTERACTION_PARAM \
     2>&1)
 
-# Show result
-user_message "$RESULT" 0
+status=$?
+# show result
+user_message "$result" 0
 
-hasError=$(echo "$RESULT" | grep -E "(failed|fatal|Error|Exception|Problem)" 2>&1)
-if [ "$hasError" ] ; then
+if [ ${status} != 0 ] ; then
     # stop if error
     die "Package requiring failed."
 fi
 # Require target package
 COMPOSER_PROCESS_TIMEOUT=600
-RESULT=$(cd "$PACKAGE_DIR" && \
+result=$(cd "$PACKAGE_DIR" && \
     COMPOSER_PROCESS_TIMEOUT=$COMPOSER_PROCESS_TIMEOUT \
     composer update $COMPOSER_VERBOSITY_PARAM \
     $INTERACTION_PARAM \
     2>&1)
+status=$?
 # show result
-user_message "$RESULT" 0
+user_message "$result" 0
 
-hasError=$(echo "$RESULT" | grep -E "(failed|fatal|Error|Exception|Problem)" 2>&1)
-if [ "$hasError" ] ; then
+if [ ${status} != 0 ] ; then
     # stop if error
     die "Package requiring failed."
 fi
 # Check if module didn't get updates
-notInstalled=$(echo "$RESULT" | grep -E "(Nothing to install or update)" 2>&1)
+notInstalled=$(echo "$result" | grep -E "(Nothing to install or update)" 2>&1)
 if [ "$notInstalled" ] && [ "$PACKAGE_INSTALL_RUN" != "true" ] ; then
     # stop if error
     die "Nothing to install or update."
