74e9b31d83bf310d91f233769b0abf2e96582294
Andrew Roslik <andkirby@gmail.com>
2015-08-17 16:46:28 +0300

    - Updated messages verbosity level.

--- src/install/magento-permissions.sh
+++ src/install/magento-permissions.sh
@@ -3,15 +3,16 @@
 cd "$PROJECT_DIR"
 # ======== Set permissions to Magento ========
 
+user_message "Set permissions to files..." 1
 # Set owner for all files
 if [ ! -z "${PROJECT_DIR_OWNER}" ] ; then
-    user_message "Changing project directory owner to '$PROJECT_DIR_OWNER'..." 1
+    user_message "Changing project directory owner to '$PROJECT_DIR_OWNER'..." 2
     chown -R $PROJECT_DIR_OWNER $PROJECT_DIR
 fi
 
 # Set permissions for media/etc/var directories
 if [ -n "$MEDIA_DIR_PERMISSIONS" ] ; then
-    user_message "Changing media/etc/var directory permissions to '$MEDIA_DIR_PERMISSIONS'..." 1
+    user_message "Changing media/etc/var directory permissions to '$MEDIA_DIR_PERMISSIONS'..." 2
     chmod -R $MEDIA_DIR_PERMISSIONS $PROJECT_DIR/media
     chmod $MEDIA_DIR_PERMISSIONS    $PROJECT_DIR/app/etc/
     chmod -R $MEDIA_DIR_PERMISSIONS $PROJECT_DIR/var
@@ -19,7 +20,7 @@ fi
 
 # Set owner for media/etc/var directories
 if [ ! -z "$MEDIA_DIR_OWNER" ] ; then
-    user_message "Changing media/etc/var directory owner to '$MEDIA_DIR_OWNER'..." 1
+    user_message "Changing media/etc/var directory owner to '$MEDIA_DIR_OWNER'..." 2
     chown -R $MEDIA_DIR_OWNER $PROJECT_DIR/media
     chown $MEDIA_DIR_OWNER    $PROJECT_DIR/app/etc/
     chown -R $MEDIA_DIR_OWNER $PROJECT_DIR/var
