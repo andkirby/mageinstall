e4cf1ea70eaa5f7854ee490056bb1fd74c319864
Andrew Roslik <andkirby@gmail.com>
2015-08-19 18:37:05 +0300

    - Fixed case when we need to set permissions for already exists app/etc/local.xml.

--- src/install/magento-permissions.sh
+++ src/install/magento-permissions.sh
@@ -15,13 +15,20 @@ if [ -n "$MEDIA_DIR_PERMISSIONS" ] ; then
     user_message "Changing media/etc/var directory permissions to '$MEDIA_DIR_PERMISSIONS'..." 2
     chmod -R $MEDIA_DIR_PERMISSIONS $PROJECT_DIR/media
     chmod $MEDIA_DIR_PERMISSIONS    $PROJECT_DIR/app/etc/
+    if [ -f $PROJECT_DIR/app/etc/local.xml ]; then
+        chmod $MEDIA_DIR_PERMISSIONS    $PROJECT_DIR/app/etc/local.xml
+    fi
     chmod -R $MEDIA_DIR_PERMISSIONS $PROJECT_DIR/var
 fi
 
 # Set owner for media/etc/var directories
 if [ ! -z "$MEDIA_DIR_OWNER" ] ; then
     user_message "Changing media/etc/var directory owner to '$MEDIA_DIR_OWNER'..." 2
+
     chown -R $MEDIA_DIR_OWNER $PROJECT_DIR/media
     chown $MEDIA_DIR_OWNER    $PROJECT_DIR/app/etc/
+    if [ -f $PROJECT_DIR/app/etc/local.xml ]; then
+        chown $MEDIA_DIR_OWNER    $PROJECT_DIR/app/etc/local.xml
+    fi
     chown -R $MEDIA_DIR_OWNER $PROJECT_DIR/var
 fi
