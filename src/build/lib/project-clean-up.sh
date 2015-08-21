17028312f422bac38cac9b0ee31394c79aa1d1e2
Andrew Roslik <andkirby@gmail.com>
2015-08-21 04:05:21 +0300

    - Show errors when it's possible but not important.

--- src/build/lib/project-clean-up.sh
+++ src/build/lib/project-clean-up.sh
@@ -20,9 +20,14 @@ if [ "$REFRESH_ALL" = "true" ] || [ ! -f "$PROJECT_DIR/app/Mage.php" ] ; then
     user_message 'Cleaning up completed.' 1
 elif [ "$PACKAGE_INSTALL_RUN" = "true" ] ; then
     user_message "Clean up project directory..." 1
+
+    set +e # possible error, just show it
+    # TODO remove enabling errors
     filesToRemove=$(cd "$PROJECT_DIR" && find . -type f -print0 |
         grep -Fxvz -f <(cd "$MAGENTO_DIR" && find . -type f) |
         xargs -0 rm 2>&1)
+    set -e #enable errors back
+
     cd "$PROJECT_DIR" && find . -empty -type d -delete
     user_message 'Cleaning up completed.' 1
 fi
