58bca18fbff1abedc71557db6b16442a9e2965da
Andrew Roslik <andkirby@gmail.com>
2015-09-18 22:26:21 +0300

    - Added dialog function for yes/no cases.

--- src/lib/function.sh
+++ src/lib/function.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Dialog answer keys
+[ -z ${DIALOG_YES_ANSWER} ] && DIALOG_YES_ANSWER='y'
+[ -z ${DIALOG_NO_ANSWER} ] && DIALOG_NO_ANSWER='n'
+readonly ${DIALOG_YES_ANSWER}
+readonly ${DIALOG_NO_ANSWER}
+
 # set verbosity modes
 declare -A VERBOSITY_MODE
 VERBOSITY_MODE['silent']=-1
@@ -12,7 +18,7 @@ readonly VERBOSITY_MODE
 # Set boolean value to a variable
 function setBoolean {
     local v
-    if (( $# != 2 )); then
+    if (($# != 2)); then
         die "Improper setBoolean() usage";
     fi
 
@@ -20,7 +26,7 @@ function setBoolean {
         TRUE | true | yes | YES | 1) v=true ;;
         FALSE | false | no | NO | 0 | "") v=false ;;
         *) die "Unknown boolean value \"$2\". Please use 'true|false' or 'yes|no' or '1|0' or '' for 'false'.";
-            ;;
+        ;;
     esac
 
     eval $1=$v
@@ -31,17 +37,17 @@ function die {
     local error_code verbosity
 
     verbosity=${VERBOSITY_LEVEL}
-    if [ -z ${verbosity} ] ; then
+    if [ -z ${verbosity} ]; then
         verbosity=${VERBOSITY_MODE['off']}
     fi
 
     echo "Error: ${1}"
 
     # show backtrace
-    if [ ${verbosity} = ${VERBOSITY_MODE['debug']} ] ; then
+    if [ ${verbosity} = ${VERBOSITY_MODE['debug']} ]; then
         local frame=0
         while caller "$frame"; do
-            ((frame++));
+            ((frame ++));
         done
     fi
 
@@ -60,15 +66,15 @@ function user_message {
     message=${1}
     min_verbosity=${2}
 
-    if [ -z ${min_verbosity} ] ; then
+    if [ -z ${min_verbosity} ]; then
         min_verbosity=${VERBOSITY_MODE['off']}
     fi
 
-    if [ -z ${verbosity} ] ; then
+    if [ -z ${verbosity} ]; then
         verbosity=${VERBOSITY_MODE['off']}
     fi
 
-    if [ ${verbosity} -ge ${min_verbosity} ] ; then
+    if [ ${verbosity} -ge ${min_verbosity} ]; then
         printf "${message}\n"
     fi
 }
@@ -84,8 +90,86 @@ function check_status {
     elif [ -z "${status}" ]; then
         die "Status code is not set."
     fi
-    if [ -z "${2}" ] ; then
+    if [ -z "${2}" ]; then
         die "Code: ${status}" ${status}
     fi
     exit ${status}
+}
+
+# Simple yes/no dialog question\
+#
+# e.g.: $(dialog_yes_no 'Would like repeat?' 'n' 3)
+#
+# string    Result variable name
+# string question
+# string default
+# int mistakes_count = 3
+# string yes_answer = 'y'
+# string no_answer = 'n'
+function dialog_yes_no {
+    local question default max_mistakes_count mistakes_count \
+        value \
+        max_mistakes_count_default \
+        yes_answer \
+        no_answer
+    local ___result
+
+    if [ $# -lt 2 ]; then
+        die "Improper dialog_yes_no() usage";
+    fi
+
+    # set question
+    question=$2
+
+    # default answer on empty input
+    default="$3"
+
+    # set yes/no answers
+    yes_answer=$5
+    no_answer=$6
+    [ -z ${yes_answer} ] && yes_answer=${DIALOG_YES_ANSWER}
+    [ -z ${no_answer} ] && no_answer=${DIALOG_NO_ANSWER}
+
+    if [ -n "${default}" ]; then
+        if [ "${default}" != "${no_answer}" ] && [ "${default}" != "${yes_answer}" ]; then
+            die 'Default value does not mutch with answers.'
+        fi
+    fi
+
+    # max mistakes
+    value=$(( $4 + 0 ))
+    max_mistakes_count=3 # default value
+    max_mistakes_count=$(( 0 != ${value} ? ${value} : ${max_mistakes_count} ))
+
+    mistake=0
+    while true
+    do
+        [ ${mistake} -ge ${max_mistakes_count} ] && die 'Max mistakes succeed.'
+
+        if [ -n "${default}" ]; then
+            printf "${question} (${yes_answer}|${no_answer}) [${default}] : "
+        else
+            printf "${question} (${yes_answer}|${no_answer}) : "
+        fi
+        read answer
+
+        [ -z ${answer} ] && answer=${default}
+
+        case ${answer} in
+            ${yes_answer})
+                ___result=1
+                break
+            ;;
+            ${no_answer})
+                ___result=''
+                break
+            ;;
+            *)
+                ((mistake ++));
+                continue
+            ;;
+        esac
+    done
+
+    eval $1=${___result}
 }
