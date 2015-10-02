38f9158f7b46138f49ebc6fe638e4a11c8256ce1
Andrew Roslik <andkirby@gmail.com>
2015-10-02 12:45:32 +0300

    - Added running PHP scripts after and before sample data step.

--- src/install/magento-sample-data.sh
+++ src/install/magento-sample-data.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
+used=''
+# Run php script before
+if [ true = "$SAMPLE_DATA_SQL_RUN" ] && [ -d "$PROJECT_DIR/shell/.mageinstall/install/sample-data/before/" ] ; then
+    user_message "Running PHP scripts before sample data step..." 0
+    user_message "Target directory shell/.mageinstall/install/sample-data/before." 2
+    for php_file in "$PROJECT_DIR/shell/.mageinstall/install/sample-data/before/*.php"; do
+        user_message "Running ${php_file}..." 2
+        ${PHP_BIN} -f ${php_file}
+    done
+fi
+
 # ======= Install Sample SQL Data =======
 if [ true = "$SAMPLE_DATA_SQL_RUN" ] && [ -d "$SAMPLE_DATA_DIR/sample" ]
 then
+    used=1
     user_message "Installing sample data SQL files..." 0
     for SQL_FILE in $SAMPLE_DATA_DIR/sample/*.sql; do
         $DB_CONNECT_COMMAND -h$DB_HOST $DB_NAME < $SQL_FILE
@@ -14,6 +26,7 @@ fi
 # ======= Install Sample Data files =======
 if [ true = "$SAMPLE_DATA_MEDIA_RUN" ] && [ -d "$SAMPLE_DATA_DIR/sample" ]
 then
+    used=1
     user_message "Copying sample data files..." 0
     if [ "$INSTALL_RUN" = "true" ] && [ -f "$SAMPLE_DATA_DIR/sample"/app/etc/local.xml ] ; then
         user_message "Notice: You going to copy 'local.xml' file. Installation will be ignored." 1
@@ -33,4 +46,14 @@ then
     done
 else
     user_message "Skipped copying sample data files." 1
+fi
+
+# Run php script after
+if [ "$used" ] && [ -d "$PROJECT_DIR/shell/.mageinstall/install/sample-data/after/" ] ; then
+    user_message "Running PHP scripts after sample data step..." 0
+    user_message "Target directory shell/.mageinstall/install/sample-data/after." 2
+    for php_file in "$PROJECT_DIR/shell/.mageinstall/install/sample-data/after/*.php"; do
+        user_message "Running ${php_file}..." 2
+        ${PHP_BIN} -f ${php_file}
+    done
 fi
