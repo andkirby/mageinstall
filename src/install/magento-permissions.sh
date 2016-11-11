37057ca63cdd3f66dbe77261c9c8d5d07dc0da0e
Andrew Roslik <andkirby@gmail.com>
2016-11-11 19:38:51 +0200

    Added reseting permissions.

--- src/install/magento-permissions.sh
+++ src/install/magento-permissions.sh
@@ -10,7 +10,22 @@ if [ ! -z "${PROJECT_DIR_OWNER}" ] ; then
     chown -R $PROJECT_DIR_OWNER $PROJECT_DIR
 fi
 
-# Set permissions for media/etc/var directories
+# == reset permissions ==
+# Drop all permissions for "group" and "others"
+chmod -R go-wx $PROJECT_DIR
+chmod -R o-r $PROJECT_DIR
+# traversing is accessed for all
+chmod -R +X $PROJECT_DIR
+# user can read and write everything
+chmod -R u+rw $PROJECT_DIR
+# group can read only
+chmod -R g+r $PROJECT_DIR/*
+# var, media, and etc are allowed to change for group
+chmod -R g+w $PROJECT_DIR/var \
+             $PROJECT_DIR/media
+chmod g+w $PROJECT_DIR/app/etc
+
+# Set custom permissions
 if [ -n "$MEDIA_DIR_PERMISSIONS" ] ; then
     user_message "Changing media/etc/var directory permissions to '$MEDIA_DIR_PERMISSIONS'..." 2
     chmod -R $MEDIA_DIR_PERMISSIONS $PROJECT_DIR/media
@@ -32,3 +47,19 @@ if [ ! -z "$MEDIA_DIR_OWNER" ] ; then
     fi
     chown -R $MEDIA_DIR_OWNER $PROJECT_DIR/var
 fi
+
+# Define owners
+chown -R youruser:nginx $PROJECT_DIR
+# Drop all permissions for "group" and "others"
+chmod -R go-wx $PROJECT_DIR
+chmod -R o-r $PROJECT_DIR
+# traversing is accessed for all
+chmod -R +X $PROJECT_DIR
+# user can read and write everything
+chmod -R u+rw $PROJECT_DIR
+# group can read only
+chmod -R g+r $PROJECT_DIR/*
+# var, media, and etc are allowed to change for group
+chmod -R g+w $PROJECT_DIR/var \
+             $PROJECT_DIR/media
+chmod g+w $PROJECT_DIR/app/etc
