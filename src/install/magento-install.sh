ca094ac44872de60d55552ac8f8aeb106191ba87
Andrew Roslik <andkirby@gmail.com>
2016-12-27 17:18:53 +0200

    Added vars for last/first name of admin. Refactored code.

--- src/install/magento-install.sh
+++ src/install/magento-install.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
 
-cd "$PROJECT_DIR"
+cd "${PROJECT_DIR}"
 # ======== Install Magento ========
-if [ "$INSTALL_RUN" = true ]
+if [ "${INSTALL_RUN}" = true ]
 then
     user_message "Start installing Magento..." 0
-    if [ ! -f "$PROJECT_DIR/install.php" ] ; then
+    if [ ! -f "${PROJECT_DIR}/install.php" ] ; then
         die "There are no Magento files."
     fi
 
     start=$(date +%s)
-    result=$($PHP_BIN -f "$SRC_DIR"/install/lib/install.php -- \
-            "$PROJECT_DIR" \
+    result=$(${PHP_BIN} -f "${SRC_DIR}"/install/lib/install.php -- \
+            "${PROJECT_DIR}" \
             --license_agreement_accepted "yes" \
             --locale "en_US" \
             --timezone "America/Los_Angeles" \
             --default_currency "USD" \
-            --db_host "$DB_HOST" \
-            --db_name "$DB_NAME" \
-            --db_user "$DB_USER" \
-            --db_pass "$DB_PASSWORD" \
-            --url "http://$PROJECT_DOMAIN/" \
-            --use_rewrites "$USE_REWRITES" \
+            --db_host "${DB_HOST}" \
+            --db_name "${DB_NAME}" \
+            --db_user "${DB_USER}" \
+            --db_pass "${DB_PASSWORD}" \
+            --url "http://${PROJECT_DOMAIN}/" \
+            --use_rewrites "${USE_REWRITES}" \
             --use_secure "yes" \
-            --secure_base_url "$PROTOCOL_SECURED://$PROJECT_DOMAIN/" \
+            --secure_base_url "${PROTOCOL_SECURED}://${PROJECT_DOMAIN}/" \
             --use_secure_admin "yes" \
-            --admin_firstname "Admin" \
-            --admin_lastname "MageInstall" \
-            --admin_email "$ADMIN_EMAIL" \
-            --admin_username "$ADMIN_USERNAME" \
-            --admin_password "$ADMIN_PASSWORD" \
+            --admin_firstname "${ADMIN_FIRSTNAME:-Admin}" \
+            --admin_lastname "${ADMIN_LASTNAME:-Default}" \
+            --admin_email "${ADMIN_EMAIL}" \
+            --admin_username "${ADMIN_USERNAME}" \
+            --admin_password "${ADMIN_PASSWORD}" \
             --skip_url_validation "yes" 2>&1)
 
     status=$?
@@ -43,6 +43,6 @@ then
     if [ ${status} != 0 ] ; then
         die "Magento installation failed."
     fi
-    user_message "Magento has been installed for domain http://$PROJECT_DOMAIN/." 0
+    user_message "Magento has been installed for domain http://${PROJECT_DOMAIN}/." 0
     user_message "Installing took $diff seconds." 0
 fi
